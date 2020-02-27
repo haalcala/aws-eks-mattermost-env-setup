d5f6738705a5a976adfc342717fb9ec1a110b3a4
Harold Alcala <harold.dev@gmail.com>
2020-02-28 07:09:40 +0800

    Readable comments

--- mattermost-env-setup-stage-2/start_db_import.sh
+++ mattermost-env-setup-stage-2/start_db_import.sh
@@ -61,6 +61,7 @@ echo "mysqldump --no-data -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT -u$LOCAL_DB_USER -p$
 cp $SCHEMA_FILE  $SCHEMA_FILE_MYISAM
 
 for table in $TABLES; do
+	echo "----==== Dumping Schema ====----"
 	DUMP_FILE=$DUMP_FOLDER/${LOCAL_DB_NAME}-${table}.sql
 
 	echo \$DUMP_FILE $DUMP_FILE
@@ -68,7 +69,8 @@ for table in $TABLES; do
 	_start=`date '+%s'`
 	mysqldump --skip-lock-tables --skip-add-locks --skip-add-drop-table --no-create-info -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT -u$LOCAL_DB_USER -p$LOCAL_DB_PASS $LOCAL_DB_NAME $table > $DUMP_FILE
 	_end=`date '+%s'`
-	echo "mysqldump --skip-lock-tables --skip-add-locks --skip-add-drop-table --no-create-info -h$LOCAL_DB_HOST -P$LOCAL_DB_PORT -u$LOCAL_DB_USER -p$LOCAL_DB_PASS $LOCAL_DB_NAME $table \> $DUMP_FILE <<-- elapsed time $(expr $_end - $_start)"
+	echo "----==== Dumping table: $table to $DUMP_FILE ..."
+	echo "----==== Dumping table: $table to $DUMP_FILE <<-- elapsed time $(expr $_end - $_start)"
 
 	# sed -e "s/^CREATE TABLE \`\($table\)\`.*/CREATE TABLE \`\1_I\`/g" $SCHEMA_FILE >  $SCHEMA_FILE_MYISAM.tmp
 	sed -e "s/\`\($table\)\`/\`\1_I\`/g" $SCHEMA_FILE_MYISAM >  $SCHEMA_FILE_MYISAM.tmp
@@ -95,23 +97,28 @@ _end=`date '+%s'`
 echo "mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME \< $SCHEMA_FILE_MYISAM <<-- elapsed time $(expr $_end - $_start)"
 
 for table in $TABLES; do
+	echo
+	echo "---------------------------------------------------------------------------------"
 	DUMP_FILE=$DUMP_FOLDER/${LOCAL_DB_NAME}-${table}.sql
 
 	_start=`date '+%s'`
 	mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME < $DUMP_FILE
 	_end=`date '+%s'`
-	echo "mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME \< $DUMP_FILE <<-- elapsed time $(expr $_end - $_start)"
+	echo "----==== Restoring $DUMP_FILE ..."
+	echo "----==== Restoring $DUMP_FILE <<-- elapsed time $(expr $_end - $_start)"
 
 	_start=`date '+%s'`
 	mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "delete from $table"
 	_end=`date '+%s'`
-	echo "mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "delete from $table" <<-- elapsed time $(expr $_end - $_start)"
+	echo "----==== Deleting table: $table ..."
+	echo "----==== Deleting table: $table <<-- elapsed time $(expr $_end - $_start)"
 	_start=`date '+%s'`
 	mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "insert into $table select * from ${table}_I"
 	_end=`date '+%s'`
-	echo "mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "insert into $table select \* from ${table}_I" <<-- elapsed time $(expr $_end - $_start)"
+	echo "----==== insert into $table select \* from ${table}_I ..."
+	echo "----==== insert into $table select \* from ${table}_I" <<-- elapsed time $(expr $_end - $_start)"
 	_start=`date '+%s'`
 	mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "drop table ${table}_I"
 	_end=`date '+%s'`
-	echo "mysql -h$DEST_DB_HOST -P$DEST_DB_PORT -u$DEST_DB_USER -p$DEST_DB_PASS $DEST_DB_NAME -e "drop table ${table}_I" <<-- elapsed time $(expr $_end - $_start)"
+	echo "----==== drop table ${table}_I <<-- elapsed time $(expr $_end - $_start)"
 done
