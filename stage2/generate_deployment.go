43c038adba24c068f6204462b3b429c1a3944749
Harold Alcala <harold.dev@gmail.com>
2021-01-19 10:36:51 +0800

    Metricbeat
    - config template

--- stage2/generate_deployment.go
+++ stage2/generate_deployment.go
@@ -30,12 +30,13 @@ func GenerateDeploymentFiles(baseDir string) {
 	_tokens := append(tokens, &Token{Key: "__NGINX_MM_DOMAINS__", Value: domain_conf})
 	_tokens = append(_tokens, &Token{Key: "__ALB_DOMAIN_RULES__", Value: alb_domain_conf})
 
-	ProcessTemplate("./deploy-nginx-router.yaml.template", baseDir+"/deploy-nginx-router.yaml", _tokens, 0666)
-	ProcessTemplate("./deploy-aws-alb.yaml.template", baseDir+"/deploy-aws-alb.yaml", _tokens, 0666)
-	ProcessTemplate("./deploy-smtp.yaml.template", baseDir+"/deploy-smtp.yaml", _tokens, 0666)
-	ProcessTemplate("./deploy-push-proxy.yaml.template", baseDir+"/deploy-push-proxy.yaml", _tokens, 0666)
-	ProcessTemplate("./deploy-redis.yaml.template", baseDir+"/deploy-redis.yaml", _tokens, 0666)
-	ProcessTemplate("./deploy-vid-oauth-wrapper.yaml.template", baseDir+"/deploy-vid-oauth-wrapper.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-nginx-router.yaml.template", baseDir+"/deploy-nginx-router.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-aws-alb.yaml.template", baseDir+"/deploy-aws-alb.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-smtp.yaml.template", baseDir+"/deploy-smtp.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-push-proxy.yaml.template", baseDir+"/deploy-push-proxy.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-redis.yaml.template", baseDir+"/deploy-redis.yaml", _tokens, 0666)
+	ProcessTemplate("deploy-vid-oauth-wrapper.yaml.template", baseDir+"/deploy-vid-oauth-wrapper.yaml", _tokens, 0666)
+	ProcessTemplate("configmap-metricbeat.yaml.template", baseDir+"/configmap-metricbeat.yaml", _tokens, 0666)
 }
 
 func main() {
